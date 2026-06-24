5aff287801bedabd901ff93072b8cdd5eeed54da
FFernandezORT <41672000@comunidadort.edu.ar>
2026-06-24 18:58:41 -0300

    authentication process

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import userController from "../container/userContainer.js";
+import { authenticate, isAdmin } from "../middlewares/auth.js";
 
 const userRoutes= Router();
 
 
-userRoutes.get("/me", userController.me);
-userRoutes.get("/:id", userController.getUserById);
-userRoutes.post("/", userController.createUser);
-userRoutes.post("/login", userController.login);
-userRoutes.put("/:id", userController.updateUser);
-userRoutes.delete("/:id", userController.deleteUser);
+userRoutes.get("/me", authenticate, userController.me);
+userRoutes.get("/:id", authenticate, userController.getUserById);
+userRoutes.post("/", authenticate, userController.createUser);
+userRoutes.post("/login", authenticate, userController.login);
+userRoutes.put("/:id", authenticate, userController.updateUser);
+userRoutes.delete("/:id", authenticate, userController.deleteUser);
 
 export default userRoutes;
